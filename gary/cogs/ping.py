bb373c67fb597a68b7657c1830162b3d86983f89
Cypheriel <61402923+Cypheriel@users.noreply.github.com>
2024-02-22 17:11:30 -0700

    fix: change how latency is tested and reported

--- gary/cogs/ping.py
+++ gary/cogs/ping.py
@@ -1,4 +1,6 @@
 """Module containing the Ping cog."""
+import asyncio
+
 from discord import ApplicationContext, Bot, slash_command
 from discord.ext.commands import Cog
 
@@ -8,11 +10,15 @@ class Ping(Cog):
 
     @slash_command()
     async def ping(self: "Ping", ctx: ApplicationContext) -> None:
-        """Report the bot's latency."""
-        latency = round(ctx.bot.latency * 1000, 2)
+        """Report the bot's average latency."""
         await ctx.trigger_typing()
 
-        await ctx.respond(f"Pong! Latency: {latency:.2f}ms")
+        total_latency = 0
+        for _ in range(5):
+            total_latency += ctx.bot.latency
+            await asyncio.sleep(0.5)
+
+        await ctx.respond(f"Pong! Average latency: {total_latency / 5:.2f}ms")
 
 
 def setup(bot: Bot) -> None:
